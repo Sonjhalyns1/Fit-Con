95bf085636d58a98dd5f12caacd90b2de1be2021
sonjhalyns augustin <sonjhalynsaugustin@gmail.com>
2023-12-29 08:17:31 -0500

    first connect added the colors, and installed the tailwind package

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { COLORS } from './constants/theme';
+import tw from "twrnc";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`bg-[${COLORS.primary}] flex flex-1 items-center justify-center`}>
+      <Text S>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ constants/theme.js
@@ -0,0 +1,9 @@
+const COLORS = {
+    primary: "#fefae0",
+    green: "#606c38",
+    dark: "#283618",
+    brown: "#dda15e",
+    darkBrown: "#bc6c25"
+}
+
+export {COLORS}
